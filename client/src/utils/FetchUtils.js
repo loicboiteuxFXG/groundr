8130b11c90f2b1c0f196b1978f6a2c6a2abe6987
Loïc Boiteux <loboi002@edu.cegepgarneau.ca>
2024-02-19 11:59:08 -0500

    FEAT: Implémentaion de la gestion dess requêtes vers le serveur backend

--- client/src/utils/FetchUtils.js
+++ client/src/utils/FetchUtils.js
@@ -1,4 +1,18 @@
 export const fetchDemo = async () => {
-    const res = await fetch('http://localhost:3001');
+    const res = await fetch('http://localhost:3001/get');
     return res.json();
+}
+
+
+export const postDemo = async (data) => {
+    const serializedBody = JSON.stringify(data); // (2) in notes below
+    console.log(serializedBody);
+    const fetchOptions = { // (3)
+        method: 'POST', // (4)
+        body: serializedBody, // (5)
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    fetch('http://localhost:3001/post', fetchOptions); // We pass in the options here to configure our fetch request
 }
--- server/index.js
+++ server/index.js
@@ -3,13 +3,24 @@ const cors = require('cors')
 const app = express()
 const port = 3001
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
+
+
+app.get('/get', async (req, res) => {
     const response = await fetch('https://catfact.ninja/fact');
     const body = await response.json();
     res.send(body);
+});
+
+
+app.post('/post', async (req, res) => {
+    const data = req.body;
+    console.log(data); // always good practice to verify your data is coming through in the shape you expect it to be in.
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
