9cc2fae5daf7833435913c1cb63dc3ce0844bf83
Loïc Boiteux <loboi002@edu.cegepgarneau.ca>
2024-02-25 23:37:05 -0500

    FEAT: Ajout du component de modification de profile

---
+++ client/src/components/ProfileEdit/ProfileEdit.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+
+const ProfileEdit = () => {
+    const [gender, setGender] = useState("M");
+    const [orientation, setOrientation] = useState("M");
+    const [DoB, setDoB] = useState(Date());
+    const [file, setFile] = useState()
+
+    function handleChange(event) {
+        setFile(event.target.files[0])
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+
+        postUpdateUser()
+    };
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Date de naissance:
+                <input type='date' onChange={e => setDoB(e.target.value)} value={DoB} />
+            </label>
+            <label>Genre
+                <select name="gender" value={gender} onChange={e => setGender(e.target.value)}>
+                    <option value="M">Homme</option>
+                    <option value="F">Femme</option>
+                    <option value="O">Autre</option>
+                </select>
+            </label>
+            <label>Je recherche
+                <select name="orientation" value={orientation} onChange={e => setOrientation(e.target.value)}>
+                    <option value="M">Homme</option>
+                    <option value="F">Femme</option>
+                    <option value="A">Tout</option>
+                    <option value="O">Autre</option>
+                </select>
+            </label>
+            <label>
+                <input type="file" accept="image/png, image/jpg, image/jpeg" onChange={handleChange}/>
+            </label>
+            <input type="submit"/>
+        </form>
+    );
+}
+
+export default ProfileEdit;
