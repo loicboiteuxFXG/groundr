144d38eea9aca0917722995510db55a30c0d747c
Adam Demers <addem001@edu.cegepgarneau.ca>
2024-02-26 00:10:35 -0500

    Implémentation des styles de base pour l'accueil

--- client/src/pages/HomeLayout/HomeLayout.jsx
+++ client/src/pages/HomeLayout/HomeLayout.jsx
@@ -4,11 +4,21 @@ import './HomeLayout.css'
 const HomeLayout = () => {
     return (
         <>
-            <h1>Page principale</h1>
-            <Link to="swipe">Swipe</Link>
-            <Link to="settings">Settings</Link>
-            <Link to="chat">Chat</Link>
-            <Outlet/>
+            <div className="page-layout">
+                <div className='sidebar'>
+                    <div>
+                        <h1>LOGO</h1>
+                        <Link to="swipe" className="btnGround">Let's Ground!</Link>
+                        <Link to="chat">Chat</Link>
+                    </div>
+                    <div>
+                        <Link to="settings">Settings</Link>
+                    </div>
+                </div>
+                <div className="content">
+                    <Outlet/>
+                </div>
+            </div>
             <Footer/>
         </>
     );
