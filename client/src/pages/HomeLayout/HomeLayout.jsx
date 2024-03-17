285cc3e5ec4cd09aed498abf753428a01ca46acc
Adam Demers <addem001@edu.cegepgarneau.ca>
2024-03-16 21:10:48 -0400

    FEAT: Ajout du bouton de l'utilisateur sur la page d'accueil

--- client/src/pages/HomeLayout/HomeLayout.jsx
+++ client/src/pages/HomeLayout/HomeLayout.jsx
@@ -1,20 +1,48 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer";
 import '../../styles.css'
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
+import ProfileButton from "../../components/ProfileButton";
+import axios from "axios";
 
 const HomeLayout = () => {
     const navigate = useNavigate();
 
+    const [connectedUser, setConnectedUser] = useState({
+        firstName: 'Chargement',
+        lastName: '',
+        pfpURL: 'default-user.png'
+    })
+
+    let fullname
+    let pfp
 
     useEffect(() => {
         document.title = "Accueil | GroundR";
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/user/get-user-token', {
+                    headers: {
+                        "Authorization": `Bearer ${JSON.parse(localStorage.getItem("usertoken"))}`
+                    }
+                });
+                setConnectedUser(response.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchData();
 
         const token = JSON.parse(localStorage.getItem("usertoken"));
         if (!token) {
             navigate('/account/login');
         }
     }, []);
+
+    fullname = connectedUser.firstName + " " + connectedUser.lastName
+    pfp = connectedUser.pfpURL
 
     const LogoutButton = () => {
 
@@ -37,6 +65,7 @@ const HomeLayout = () => {
                 <div className='sidebar'>
                     <div>
                         <h1 className="homeTitle"><img src={require('../../images/logo_nobackground.png')} alt="GroundR" /></h1>
+                        <ProfileButton name={fullname} pfp={pfp}/>
                         <Link to="swipe" className="btnGround">Let's Ground!</Link>
                         <Link to="chat">Chat</Link>
                     </div>
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,21 +1,17 @@
 'use strict';
 
 const express = require('express');
-const { GetUser } = require('../utils/MongoUtils');
+const { GetUser, CreateUser} = require('../utils/MongoUtils');
 const router = express.Router();
 
 const { upload } = require('express-fileupload');
 
 const { ValidateSignup } = require('../utils/Validation')
+const {isArray} = require("util");
+const userController = require("../controllers/userController")
+const {isAuth} = require("../middleware");
 
-router.post('/create', (req, res) => {
-    
-    //console.info(ValidateSignup(req.body))
-    // return
-    MongoUtils.CreateUser(req.body)
-        .then(user => {console.dir(user)})
-        .catch(err => console.error(err));
-});
+router.get('/get-user-token', isAuth, userController.GetUserToken)
 
 router.get('/:id', (req, res) => {
     return res.send({})
