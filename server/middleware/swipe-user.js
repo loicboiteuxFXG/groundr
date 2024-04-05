63bce2508ab2be56577f7243227dddb4148d392a
Loïc Boiteux <loboi002@edu.cegepgarneau.ca>
2024-04-05 08:23:55 -0400

    FEAT: Intégration du modèle Ground dans le backend

--- server/middleware/swipe-user.js
+++ server/middleware/swipe-user.js
@@ -1,36 +1,35 @@
-const { GetUser, GetAllGrounds, CreateGround, UpdateGround, DeleteGround } = require('../utils/MongoUtils');
-const { ObjectId } = require("mongodb");
+const Ground = require("../models/ground");
 
 const swipeUser = async (req, res, next) => {
     const swipeStatus = req.body.swipeStatus;
     const swipedUser = req.body.swipedUser;
 
-    const fetchedGroundsList = await GetAllGrounds(
-        {
-            $or: [
-                { sender: new ObjectId(req.user._id), receiver: new ObjectId(swipedUser._id) },
-                { sender: new ObjectId(swipedUser._id), receiver: new ObjectId(req.user._id) }],
-            status: { $not: { $eq: "accepted" } }
-        });
+    const fetchedGroundsList = await Ground.find({
+        $or: [
+            { sender: req.user._id, receiver: swipedUser._id },
+            { sender:swipedUser._id, receiver: req.user._id }],
+        status: { $not: { $eq: "accepted" } }
+    });
 
 
     const fetchedGround = fetchedGroundsList[0];
     if (!fetchedGround) {
         // First swipe
         console.log("Création d'un Ground.");
-        await CreateGround(req.user._id, swipedUser._id, swipeStatus);
+        const newGround = new Ground({sender: req.user._id, receiver: swipedUser._id, status: swipeStatus})
+        await newGround.save()
         res.send({action: "create", match: false});
     } else if (fetchedGround.sender.equals(swipedUser._id)) {
         // Ground commun
         if (swipeStatus !== "dislike") {
             // MATCH
             console.log("MATCH! Mise à jour du Ground.");
-            await UpdateGround(fetchedGround._id, { status: "common" });
+            await Ground.findByIdAndUpdate(fetchedGround._id, { status: "common" });
             res.send({action: "update", match: true, user: swipedUser});
         } else {
             // DELETE
             console.log("Pas de match. Suppression du ground");
-            await DeleteGround(fetchedGround._id);
+            await Ground.findByIdAndDelete(fetchedGround._id);
             res.send({action: "delete", match: false});
         }
     } else if (fetchedGround.sender.equals(req.user._id)) {
@@ -39,7 +38,7 @@ const swipeUser = async (req, res, next) => {
         if (swipeStatus === "dislike") {
             // DELETE
             console.log("Disliké(e) plus tard. Supression du Ground.");
-            await DeleteGround(fetchedGround._id);
+            await Ground.findByIdAndDelete(fetchedGround._id);
             res.send({action: "delete", match: false});
         } else {
             res.send({action: "none", match: false});
